adfec2d576aecec299c86e26bcfc27e19b6390ab
Carlos Henrique Tourinho Santana <Henriquetourinho@proton.me>
2024-01-18 10:16:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Currículo Digital - Carlos Henrique Tourinho Santana</title>
+<meta name="robots" content="noindex" />
+<meta http-equiv="Content-Language" content="pt-br">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="author" content="Henrique Tourinho">
+<link rel="stylesheet" href="/../sistema/css/estilo.css">
+<link rel="shortcut icon" href="/media/favicon.svg" />
+
+
+</head>
+<body>
+<header>
+<p style="text-align: left;color:#111;font-size:12px;"><a class="page-link" href="/arquivos/14-de-janeiro-de-2024.pdf" target="_blank">Vizualizar em PDF</a></p>
+
+</header>
+
+<div id="timeline">
+<br>
+<center>
+	<img alt="Henrique Tourinho" height="150" width="150" style="border:5px solid #111;border-radius:50%;" src="https://henriquetourinho.com.br/media/perfil.webp" title="Henrique Tourinho">
+
+</center>
+<h2>Nome</h2>
+<p>Carlos Henrique Tourinho Santana</p>
+<h2>Contato</h2>
+<p>Celular: <a href="tel:+5571984544466" target="_blank">(71) 9 8454-4466</a></p>
+<p>E-mail: <a href="mailto:henriquetourinho@proton.me" target="_blank">henriquetourinho@proton.me</a></p>
+
+<h3>Endereço</h3>
+<p>Pau da Lima, Salvador, Bahia - Brasil.</p>
+
+<h3>Sobre</h3>
+<p>Especialista em TI com expertise em sistemas e servidores, focado em WHM, WHMCS e WordPress.</p>
+
+<h2>Educação</h2>
+<p style="font-size: 17px;">Análise e Desenvolvimento de Sistemas 
+<br>
+Salvador - 1º período. 
+<br>
+Fevereiro de 2024 até dezembro de 2026. 
+<br>
+Ruy Barbosa.</p>
+
+
+
+<h3>Cursos</h3>
+<ul>
+   <li>Treinamento de Trabalho em Altura – NR 35 - Senai – 2023.</li>
+   <li>AWS na prática - Udemy / Phillipe Farias – 2022.</li>
+   <li>WordPress do Básico ao Avançado - Udemy / Ranima Comp – 2022.</li>
+   <li>Assistente Administrativo - Udemy / Samuel Branco – 2022.</li>
+ <li>Curso Básico de HTML5 & CSS3 - Slackjeff – 2022.</li>
+ <li>Gestão empresarial integrada - Sebrae – 2022.</li>
+  <li>Inteligência Emocional - Udemy / Adriano Sugimoto  – 2022.</li>
+ <li>Qualidade no atendimento ao cliente - Sebrae  – 2022.</li>
+  <li>Algoritmos e lógica de programação com Python 3 - Udemy / Matheus Battisti – 2022.</li>
+  <li>Curso Básico de Comunicação Interpessoal - Udemy / José Antônio Rosa – 2022.</li>
+    <li>Programação em Shell Script do básico ao avançado - Slackjeff – 2022.</li>
+ <li>Administração servidor VPS com Debian GNU/Linux - Slackjeff – 2022.</li>
+</ul>
+
+<h3>Experiência Profissional</h3>
+<p>RF Holding - Diretor de Operações, Administrador de Sistemas e Servidores / 2022 – atualmente.</p>
+ 
+<h3>Informações Complementares</h3>
+<p>Ambientes Linux, Shell, PHP, Python, Amazon Web Service, Digital Ocean, Wordpress & Visual Studio Code. </p>
+ 
+</div>
+
+<p style="text-align:right;color:#111;font-size:10px;">Pau da Lima, Salvador / BA – 13 de janeiro de 2024.</p>
+
+
+</body></html>
